b99ae35d3c43e065af192a99e2e9b7994db6e209
tahakhany <39022302+tahakhany@users.noreply.github.com>
2018-05-20 18:31:52 +0430

    Initial commit

---
+++ src/EquationSolver.java
@@ -0,0 +1,8 @@
+public class EquationSolver
+{
+    //solves a*x + b =0
+    public static int eq1(int a,int b)
+    {
+        return (-b/a);
+    }
+}
---
+++ src/com/company/EquationSolver.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class EquationSolver
+{
+    //solves a*x + b =0
+    public static int eq1(int a,int b)
+    {
+        return (-b/a);
+    }
+}
